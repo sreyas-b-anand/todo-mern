22f65f14a713c6833658cf65945e7598363c9218
sreyas <sreyas1045@gmail.com>
2025-02-04 18:48:38 +0530

    updated env variables

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,9 @@
-import express from 'express';
-import { loginUser, signupUser } from '../controller/userController.js';
+import express from "express";
+import { loginUser, signupUser } from "../controller/userController.js";
 
 const router = express.Router();
-
 // Login
-router.post('/login', loginUser);
-
+router.post("/login", loginUser);
 // Sign up
-router.post('/signup', signupUser);
-
+router.post("/signup", signupUser);
 export default router;
